d77687bacbe0464e9543fcd72058cce3852c29ca
Priya7797 <81359264+Priya7797@users.noreply.github.com>
2021-04-11 18:05:41 +0530

    Update radiuspiechart.js

--- radiuspiechart.js
+++ radiuspiechart.js
@@ -37,7 +37,50 @@
 			shadowRoot.appendChild(tmpl.content.cloneNode(true));
 			
 			
-// Themes begin
+
+					
+			
+		}
+
+
+        //Fired when the widget is added to the html DOM of the page
+        connectedCallback(){
+            this._firstConnection = true;
+            this.redraw();
+        }
+
+         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback(){
+        
+        }
+
+         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+		onCustomWidgetBeforeUpdate(oChangedProperties) {
+
+		}
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+		onCustomWidgetAfterUpdate(oChangedProperties) {
+            if (this._firstConnection){
+                this.redraw();
+            }
+        }
+        
+        //When the custom widget is removed from the canvas or the analytic application is closed
+        onCustomWidgetDestroy(){
+        }
+
+        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
+        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+        /*
+        onCustomWidgetResize(width, height){
+        
+        }
+        */
+
+        redraw(){
+		// Themes begin
 am4core.useTheme(am4themes_animated);
 // Themes end
 
@@ -83,48 +126,6 @@ series.hiddenState.properties.endAngle = -90;
 
 chart.legend = new am4charts.Legend();
 // end am4core.ready()
-					
-			
-		}
-
-
-        //Fired when the widget is added to the html DOM of the page
-        connectedCallback(){
-            this._firstConnection = true;
-            this.redraw();
-        }
-
-         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-        disconnectedCallback(){
-        
-        }
-
-         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-		onCustomWidgetBeforeUpdate(oChangedProperties) {
-
-		}
-
-        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-		onCustomWidgetAfterUpdate(oChangedProperties) {
-            if (this._firstConnection){
-                this.redraw();
-            }
-        }
-        
-        //When the custom widget is removed from the canvas or the analytic application is closed
-        onCustomWidgetDestroy(){
-        }
-
-        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
-        /*
-        onCustomWidgetResize(width, height){
-        
-        }
-        */
-
-        redraw(){
         }
     
     
