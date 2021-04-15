c670c28a2a1e4bfc65347e5cdc65d4703285d5ce
Pulkit Patel <50052937+pdev11@users.noreply.github.com>
2021-04-15 19:36:54 +0530

    Update newchat.js

--- newchat.js
+++ newchat.js
@@ -1,49 +1,46 @@
-(function()  {
-    let tmpl = document.createElement('template');
-    tmpl.innerHTML = `
-        <h1>Hello World</h1>
-    `;
+(function() {
+	let template = document.createElement("template");
+	template.innerHTML = `
+		<form id="form">
+			<fieldset>
+				<legend>Custom Widget Text</legend>
+				<table>
+					<tr>
+						<td>Text</td>
+						<td><input id="aps_text" type="string"></td>
+					</tr>
+				</table>
+			</fieldset>
+		</form>
+	`;
 
-    customElements.define('com-sap-sample-piechart1', class HelloWorld1 extends HTMLElement {
-
-
+	class HelloWorldAps extends HTMLElement {
 		constructor() {
-			super(); 
+			super();
 			this._shadowRoot = this.attachShadow({mode: "open"});
-            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+			this._shadowRoot.appendChild(template.content.cloneNode(true));
+			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
 		}
 
-        //Fired when the widget is added to the html DOM of the page
-        connectedCallback(){
-        }
-
-         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-        disconnectedCallback(){
-        
-        }
-
-         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-		onCustomWidgetBeforeUpdate(oChangedProperties) {
-
+		_submit(e) {
+			e.preventDefault();
+			this.dispatchEvent(new CustomEvent("propertiesChanged", {
+					detail: {
+						properties: {
+							widgetText: this.widgetText
+						}
+					}
+			}));
 		}
 
-        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-		onCustomWidgetAfterUpdate(oChangedProperties) {
-            this.redraw();
-        }
-        
-        //When the custom widget is removed from the canvas or the analytic application is closed
-        onCustomWidgetDestroy(){
-        }
+		set widgetText(newText) {
+			this._shadowRoot.getElementById("aps_text").value = newText;
+		}
 
-        
-        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default
-        /*
-        onCustomWidgetResize(width, height){
-        }
-        */
+		get widgetText() {
+			return this._shadowRoot.getElementById("aps_text").value;
+		}
+	}
 
-        redraw(){}
-    });
+customElements.define("com-sap-sample-helloworld4-aps", HelloWorldAps);
 })();
